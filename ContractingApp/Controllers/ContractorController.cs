8ae00af1be29691bf051621fbea7d38f5b48d26c
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-06-17 08:17:20 -0400

    Error handling added

--- ContractingApp/Controllers/ContractorController.cs
+++ ContractingApp/Controllers/ContractorController.cs
@@ -1,46 +1,91 @@
 ﻿using ContractingApp.Models;
 using ContractingApp.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
 
 namespace ContractingApp.Controllers
 {
     public class ContractorController : Controller
     {
         private readonly IContractorService contractService;
+        private readonly ILogger<ContractorController> logger;
 
-        public ContractorController(IContractorService contractService)
+        public ContractorController(IContractorService contractService, ILogger<ContractorController> logger)
         {
             this.contractService = contractService;
+            this.logger = logger;
         }
         public IActionResult Index()
         {
-            var contractors = contractService.GetContractors().Result;
-            return View(contractors);
+            try
+            {
+                var contractors = contractService.GetContractors().Result;
+                return View(contractors);
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation("An error occured. Please find below the error details.");
+                logger.LogError("Stack Trace {0}", ex.StackTrace);
+                logger.LogError("Error Message {0}", ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
         }
         [HttpPost]
         public IActionResult AddNewContractor(Contractor contractor)
         {
-            var numberOfRecords = contractService.AddContractor(contractor);
-            if (numberOfRecords > 0)
+            try
             {
-                TempData["message"] = "Success";
-                return RedirectToAction("Index");
+                var numberOfRecords = contractService.AddContractor(contractor);
+                if (numberOfRecords > 0)
+                {
+                    TempData["message"] = "Success";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return View();
+                logger.LogInformation("An error occured. Please find below the error details.");
+                logger.LogError("Stack Trace {0}", ex.StackTrace);
+                logger.LogError("Error Message {0}", ex.Message);
+                return RedirectToAction("Index", "Error");
             }
         }
         [HttpGet]
         public IActionResult GetContractorDetails(int Id)
         {
-            var contractor = contractService.GetContractorDetails(Id).Result;
-            return View(contractor);
+            try
+            {
+                var contractor = contractService.GetContractorDetails(Id).Result;
+                return View(contractor);
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation("An error occured. Please find below the error details.");
+                logger.LogError("Stack Trace {0}", ex.StackTrace);
+                logger.LogError("Error Message {0}", ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
         }
         [HttpGet]
         public IActionResult AddNewContractor()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation("An error occured. Please find below the error details.");
+                logger.LogError("Stack Trace {0}", ex.StackTrace);
+                logger.LogError("Error Message {0}", ex.Message);
+                return RedirectToAction("Index", "Error");
+            }
         }
     }
 }
--- ContractingApp/Repository/Implementation/ContractorRepository.cs
+++ ContractingApp/Repository/Implementation/ContractorRepository.cs
@@ -19,92 +19,164 @@ namespace ContractingApp.Repository.Implementation
 
         public int AddContractor(Contractor contractor)
         {
-            applicationContext.Contractors.Add(contractor);
-            return applicationContext.SaveChanges();
+            try
+            {
+                applicationContext.Contractors.Add(contractor);
+                return applicationContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public int AddNewContractRelation(int contractor1Id, int contractor2Id)
         {
-            applicationContext.ContractorRelations.Add(new ContractorRelations()
+            try
             {
-                Contractor1Id = contractor1Id,
-                Contractor2Id = contractor2Id
-            });
-            return applicationContext.SaveChanges();
+                applicationContext.ContractorRelations.Add(new ContractorRelations()
+                {
+                    Contractor1Id = contractor1Id,
+                    Contractor2Id = contractor2Id
+                });
+                return applicationContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public async Task<IEnumerable<Contractor>> AddNewContractRelation()
         {
-            return await GetContractors();
+            try
+            {
+                return await GetContractors();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public async Task<Contractor> GetContractorDetails(int contractorId)
         {
-            var contractors = await applicationContext.Contractors.
-                 Include(a => a.ContractorAddress)
-                .FirstOrDefaultAsync(a => a.Id == contractorId && a.IsDeleted == false);
-            return contractors;
+            try
+            {
+                var contractors = await applicationContext.Contractors.
+                        Include(a => a.ContractorAddress)
+                       .FirstOrDefaultAsync(a => a.Id == contractorId && a.IsDeleted == false);
+                return contractors;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public async Task<IEnumerable<Contractor>> GetContractors()
         {
-            //var c = applicationContext.Contractors.ToList();
-            var contractors = await applicationContext.Contractors.
-                 Include(a => a.ContractorAddress)
-                .Where(a => a.IsDeleted == false)
-                .ToListAsync();
-            return contractors;
+
+            try
+            {
+                var contractors = await applicationContext.Contractors.
+                         Include(a => a.ContractorAddress)
+                        .Where(a => a.IsDeleted == false)
+                        .ToListAsync();
+                return contractors;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public List<int> GetRelatedContractors(int contractorId)
         {
-            List<int> relatedContractorIds = new List<int>();
-            var ids = applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractorId || x.Contractor2Id == contractorId) && x.IsDeleted == false).ToList();
-            foreach (var id in ids)
+            try
             {
-                relatedContractorIds.Add(id.Contractor1Id != contractorId ? id.Contractor1Id : id.Contractor2Id);
+                List<int> relatedContractorIds = new List<int>();
+                var ids = applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractorId || x.Contractor2Id == contractorId) && x.IsDeleted == false).ToList();
+                foreach (var id in ids)
+                {
+                    relatedContractorIds.Add(id.Contractor1Id != contractorId ? id.Contractor1Id : id.Contractor2Id);
+                }
+                return relatedContractorIds;
             }
-            return relatedContractorIds;
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public string GetShortestContractingChain(int contractor1Id, int contractor2Id)
         {
-            List<string> shortestContractingChainOfContractors = new List<string>();
-            var adjacencyList = BuildAdjacencyList(contractor1Id, contractor2Id);
-            var shortestChainsIds = GetShortestContractingChainHelper(contractor1Id, contractor2Id, adjacencyList);
-            if (shortestChainsIds.Count > 0)
+            try
             {
-                shortestContractingChainOfContractors = GetNameFromIds(shortestChainsIds);
-                StringBuilder sb = new StringBuilder();
-                foreach(var s in shortestContractingChainOfContractors)
+                List<string> shortestContractingChainOfContractors = new List<string>();
+                var adjacencyList = BuildAdjacencyList(contractor1Id, contractor2Id);
+                var shortestChainsIds = GetShortestContractingChainHelper(contractor1Id, contractor2Id, adjacencyList);
+                if (shortestChainsIds.Count > 0)
                 {
-                    sb.Append(s);
-                    sb.Append("->");
+                    shortestContractingChainOfContractors = GetNameFromIds(shortestChainsIds);
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var s in shortestContractingChainOfContractors)
+                    {
+                        sb.Append(s);
+                        sb.Append("->");
+                    }
+                    sb.Remove(sb.Length - 2, 2);
+                    return sb.ToString();
                 }
-                sb.Remove(sb.Length - 2, 2);
-                return sb.ToString();
+                else
+                {
+                    return "No relations found";
+                }
             }
-            else
+            catch (System.Exception)
             {
-                return "No relations found";
+
+                throw;
             }
         }
 
         public bool IsContractorAlreadyRelated(int contractor1Id, int contractor2Id)
         {
-            var result = applicationContext.ContractorRelations.FirstOrDefault(c => (c.Contractor1Id == contractor1Id && c.Contractor2Id == contractor2Id && c.IsDeleted == false) || (c.Contractor1Id == contractor2Id && c.Contractor2Id == contractor1Id && c.IsDeleted == false));
-            if (result == null)
+            try
             {
-                return false;
+                var result = applicationContext.ContractorRelations.FirstOrDefault(c => (c.Contractor1Id == contractor1Id && c.Contractor2Id == contractor2Id && c.IsDeleted == false) || (c.Contractor1Id == contractor2Id && c.Contractor2Id == contractor1Id && c.IsDeleted == false));
+                if (result == null)
+                {
+                    return false;
+                }
+                return true;
             }
-            return true;
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         public int TerminateContractorRelation(int contractor1Id, int contractor2Id)
         {
-            var contractorRelation = applicationContext.ContractorRelations.FirstOrDefault(x => (x.Contractor1Id == contractor1Id && x.Contractor2Id == contractor2Id) || (x.Contractor1Id == contractor2Id && x.Contractor2Id == contractor1Id));
-            contractorRelation.IsDeleted = true;
-            return applicationContext.SaveChanges();
+            try
+            {
+                var contractorRelation = applicationContext.ContractorRelations.FirstOrDefault(x => (x.Contractor1Id == contractor1Id && x.Contractor2Id == contractor2Id) || (x.Contractor1Id == contractor2Id && x.Contractor2Id == contractor1Id));
+                contractorRelation.IsDeleted = true;
+                return applicationContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
         /// <summary>
         /// This is the first step of BFS to build the adjacency list
@@ -115,37 +187,45 @@ namespace ContractingApp.Repository.Implementation
         /// <returns></returns>
         private Dictionary<int, List<int>> BuildAdjacencyList(int contractor1Id, int contractor2Id)
         {
-            Dictionary<int, List<int>> adjacencyList = new Dictionary<int, List<int>>();
-            var relationsList= applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractor1Id || x.Contractor2Id == contractor1Id) && x.IsDeleted==false).ToList();
-            var relationsList2 = applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractor2Id || x.Contractor2Id == contractor2Id) && x.IsDeleted == false).ToList();
-            foreach(var r in relationsList2)
+            try
             {
-                if(!relationsList.Contains(r))
+                Dictionary<int, List<int>> adjacencyList = new Dictionary<int, List<int>>();
+                var relationsList = applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractor1Id || x.Contractor2Id == contractor1Id) && x.IsDeleted == false).ToList();
+                var relationsList2 = applicationContext.ContractorRelations.Where(x => (x.Contractor1Id == contractor2Id || x.Contractor2Id == contractor2Id) && x.IsDeleted == false).ToList();
+                foreach (var r in relationsList2)
                 {
-                    relationsList.Add(r);
+                    if (!relationsList.Contains(r))
+                    {
+                        relationsList.Add(r);
+                    }
                 }
+
+                foreach (var relation in relationsList)
+                {
+                    if (adjacencyList.ContainsKey(relation.Contractor1Id))
+                    {
+                        adjacencyList[relation.Contractor1Id].Add(relation.Contractor2Id);
+                    }
+                    else
+                    {
+                        adjacencyList.Add(relation.Contractor1Id, new List<int>() { relation.Contractor2Id });
+                    }
+                    if (adjacencyList.ContainsKey(relation.Contractor2Id))
+                    {
+                        adjacencyList[relation.Contractor2Id].Add(relation.Contractor1Id);
+                    }
+                    else
+                    {
+                        adjacencyList.Add(relation.Contractor2Id, new List<int>() { relation.Contractor1Id });
+                    }
+                }
+                return adjacencyList;
             }
-           
-            foreach (var relation in relationsList)
+            catch (System.Exception)
             {
-                if (adjacencyList.ContainsKey(relation.Contractor1Id))
-                {
-                    adjacencyList[relation.Contractor1Id].Add(relation.Contractor2Id);
-                }
-                else
-                {
-                    adjacencyList.Add(relation.Contractor1Id, new List<int>() { relation.Contractor2Id });
-                }
-                if (adjacencyList.ContainsKey(relation.Contractor2Id))
-                {
-                    adjacencyList[relation.Contractor2Id].Add(relation.Contractor1Id);
-                }
-                else
-                {
-                    adjacencyList.Add(relation.Contractor2Id, new List<int>() { relation.Contractor1Id });
-                }
+
+                throw;
             }
-            return adjacencyList;
         }
         /// <summary>
         /// Here we are using BFS to find the shortest path between two nodes of the graph
@@ -156,55 +236,63 @@ namespace ContractingApp.Repository.Implementation
         /// <returns></returns>
         private List<int> GetShortestContractingChainHelper(int contractor1Id, int contractor2Id, Dictionary<int, List<int>> adjacencyList)
         {
-           
-            List<int> path = new List<int>();
-            if (adjacencyList.Count > 0)
+
+            try
             {
-                Queue<int> queue = new Queue<int>();
-                bool[] visited = new bool[adjacencyList.Count + 1];
-
-                bool flag = false;
-                List<int?> prev = new List<int?>();
-                for (int i = 0; i < adjacencyList.Count + 1; i++)
+                List<int> path = new List<int>();
+                if (adjacencyList.Count > 0)
                 {
-                    prev.Add(null);
-                }
+                    Queue<int> queue = new Queue<int>();
+                    bool[] visited = new bool[adjacencyList.Count + 1];
 
-                queue.Enqueue(contractor1Id);
-                visited[contractor1Id] = true;
-                while (queue.Count > 0 && !flag)
-                {
-                    int size = queue.Count;
-
-                    for (int i = 0; i < size; i++)
+                    bool flag = false;
+                    List<int?> prev = new List<int?>();
+                    for (int i = 0; i < adjacencyList.Count + 1; i++)
                     {
-                        var contractorId = queue.Dequeue();
-
-                        foreach (var relatedContractorId in adjacencyList[contractorId])
-                        {
-                            if (!visited[relatedContractorId])
-                            {
-                                visited[relatedContractorId] = true;
-                                queue.Enqueue(relatedContractorId);
-
-                                prev[relatedContractorId] = contractorId;
-
-                            }
-                        }
-                        if (contractorId == contractor2Id)
-                        {
-                            flag = true;
-                            break;
-                        }
+                        prev.Add(null);
                     }
 
+                    queue.Enqueue(contractor1Id);
+                    visited[contractor1Id] = true;
+                    while (queue.Count > 0 && !flag)
+                    {
+                        int size = queue.Count;
+
+                        for (int i = 0; i < size; i++)
+                        {
+                            var contractorId = queue.Dequeue();
+
+                            foreach (var relatedContractorId in adjacencyList[contractorId])
+                            {
+                                if (!visited[relatedContractorId])
+                                {
+                                    visited[relatedContractorId] = true;
+                                    queue.Enqueue(relatedContractorId);
+
+                                    prev[relatedContractorId] = contractorId;
+
+                                }
+                            }
+                            if (contractorId == contractor2Id)
+                            {
+                                flag = true;
+                                break;
+                            }
+                        }
+
+                    }
+                    if (flag)
+                    {
+                        path = ReconstructPath(contractor1Id, contractor2Id, prev);
+                    }
                 }
-                if (flag)
-                {
-                    path = ReconstructPath(contractor1Id, contractor2Id, prev);
-                }
+                return path;
             }
-            return path;
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
         /// <summary>
         /// Once the BFS is completed this method will be called to reconstruct the path
@@ -215,26 +303,42 @@ namespace ContractingApp.Repository.Implementation
         /// <returns></returns>
         private List<int> ReconstructPath(int contractor1Id,int contractor2Id,List<int?> prev)
         {
-            List<int> path = new List<int>();
-            path.Add(contractor2Id);
-            int previousNode = contractor2Id;
-            while(prev[previousNode]!=null)
+            try
             {
-                path.Add((int)prev[previousNode]);
-                previousNode = (int)prev[previousNode];
+                List<int> path = new List<int>();
+                path.Add(contractor2Id);
+                int previousNode = contractor2Id;
+                while (prev[previousNode] != null)
+                {
+                    path.Add((int)prev[previousNode]);
+                    previousNode = (int)prev[previousNode];
+                }
+                path.Reverse();
+                return path;
             }
-            path.Reverse();
-            return path;
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
 
         private List<string> GetNameFromIds(List<int> ids)
         {
-            List<string> result = new List<string>();
-            foreach(var id in ids)
+            try
             {
-                result.Add(applicationContext.Contractors.FirstOrDefault(x => x.Id == id).Name);
+                List<string> result = new List<string>();
+                foreach (var id in ids)
+                {
+                    result.Add(applicationContext.Contractors.FirstOrDefault(x => x.Id == id).Name);
+                }
+                return result;
             }
-            return result;
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
